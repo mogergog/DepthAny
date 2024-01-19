dd76a42ada6a7d09d8fdd64ced4165da405f5473
Lihe Yang <LiheYoung@users.noreply.huggingface.co>
2024-01-19 10:20:35 +0000

    ZeroGPU (#2)
    - Use Gradio 4.x so it can work with ZeroGPU (a8d124d9c329f5d663cb7ce5f3ed6767c7655e19)
    
    
    Co-authored-by: hysts <hysts@users.noreply.huggingface.co>

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import gradio as gr
 import cv2
 import numpy as np
 from PIL import Image
+import spaces
 import torch
 import torch.nn.functional as F
 from torchvision.transforms import Compose
@@ -20,14 +21,14 @@ css = """
 #img-display-output {
     max-height: 80vh;
     }
-    
+
 """
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 model = DPT_DINOv2(encoder='vitl', features=256, out_channels=[256, 512, 1024, 1024]).to(DEVICE).eval()
 model.load_state_dict(torch.load('checkpoints/depth_anything_vitl14.pth'))
 
 title = "# Depth Anything"
-description = """Official demo for **Depth Anything: Unleashing the Power of Large-Scale Unlabeled Data**. 
+description = """Official demo for **Depth Anything: Unleashing the Power of Large-Scale Unlabeled Data**.
 
 Please refer to our [paper](), [project page](https://depth-anything.github.io), or [github](https://github.com/LiheYoung/Depth-Anything) for more details."""
 
@@ -45,38 +46,44 @@ transform = Compose([
         PrepareForNet(),
 ])
 
+
+@spaces.GPU
+@torch.no_grad()
+def predict_depth(model, image):
+    return model(image)
+
+
 with gr.Blocks(css=css) as demo:
     gr.Markdown(title)
     gr.Markdown(description)
     gr.Markdown("### Depth Prediction demo")
-    
+
     with gr.Row():
-        input_image = gr.Image(label="Input Image", type='numpy', elem_id='img-display-input').style(height="auto")
+        input_image = gr.Image(label="Input Image", type='numpy', elem_id='img-display-input')
         depth_image = gr.Image(label="Depth Map", elem_id='img-display-output')
     raw_file = gr.File(label="16-bit raw depth (can be considered as disparity)")
     submit = gr.Button("Submit")
 
     def on_submit(image):
         h, w = image.shape[:2]
-        
+
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) / 255.0
         image = transform({'image': image})['image']
         image = torch.from_numpy(image).unsqueeze(0).to(DEVICE)
-        
-        with torch.no_grad():
-            depth = model(image)
+
+        depth = predict_depth(model, image)
         depth = F.interpolate(depth[None], (h, w), mode='bilinear', align_corners=False)[0, 0]
-        
+
         raw_depth = Image.fromarray(depth.cpu().numpy().astype('uint16'))
         tmp = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
         raw_depth.save(tmp.name)
-        
+
         depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
         depth = depth.cpu().numpy().astype(np.uint8)
         colored_depth = cv2.applyColorMap(depth, cv2.COLORMAP_INFERNO)[:, :, ::-1]
-        
+
         return [colored_depth, tmp.name]
-    
+
     submit.click(on_submit, inputs=[input_image], outputs=[depth_image, raw_file])
     examples = gr.Examples(examples=["examples/flower.png", "examples/roller_coaster.png", "examples/hall.png", "examples/car.png", "examples/person.png"],
                            inputs=[input_image])
