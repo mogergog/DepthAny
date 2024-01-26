b9a7610b911db6cb3e01718c0745723a2eae0df2
Lihe Yang <LiheYoung@users.noreply.huggingface.co>
2024-01-26 08:16:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -93,57 +93,6 @@ with gr.Blocks(css=css) as demo:
         example_files = [os.path.join('examples', filename) for filename in example_files]
         examples = gr.Examples(examples=example_files, inputs=[input_image], outputs=[depth_image_slider, raw_file], fn=on_submit, cache_examples=True)
 
-    with gr.Tab("Video Depth Prediction"):
-        with gr.Row():
-            input_video = gr.Video(label="Input Video")
-        submit = gr.Button("Submit")
-        processed_video = gr.Video(label="Processed Video")
-
-        def on_submit(filename):
-            raw_video = cv2.VideoCapture(filename)
-            frame_width, frame_height = int(raw_video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(raw_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            frame_rate = int(raw_video.get(cv2.CAP_PROP_FPS))
-            output_width = frame_width * 2 + margin_width
-            
-            with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as tmpfile:
-                output_path = tmpfile.name
-            out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*"mp4v"), frame_rate, (output_width, frame_height))
-            
-            while raw_video.isOpened():
-                ret, raw_frame = raw_video.read()
-                if not ret:
-                    break
-                
-                frame = cv2.cvtColor(raw_frame, cv2.COLOR_BGR2RGB) / 255.0
-                
-                frame = transform({'image': frame})['image']
-                frame = torch.from_numpy(frame).unsqueeze(0).to(DEVICE)
-                
-                depth = predict_depth(model, frame)
-
-                depth = F.interpolate(depth[None], (frame_height, frame_width), mode='bilinear', align_corners=False)[0, 0]
-                depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
-                
-                depth = depth.cpu().numpy().astype(np.uint8)
-                depth_color = cv2.applyColorMap(depth, cv2.COLORMAP_INFERNO)
-                
-                split_region = np.ones((frame_height, margin_width, 3), dtype=np.uint8) * 255
-                combined_frame = cv2.hconcat([raw_frame, split_region, depth_color])
-                
-                out.write(combined_frame)
-            
-            raw_video.release()
-            out.release()
-            
-            return output_path
-        
-        submit.click(on_submit, inputs=[input_video], outputs=processed_video)
-
-        example_files = os.listdir('examples_video')
-        example_files.sort()
-        example_files = [os.path.join('examples_video', filename) for filename in example_files]
-        examples = gr.Examples(examples=example_files, inputs=[input_video], outputs=processed_video, fn=on_submit, cache_examples=True)
-        
 
 if __name__ == '__main__':
     demo.queue().launch()
